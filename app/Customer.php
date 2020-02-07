f785bae17877c3724c5fcd5d24ad68b61492582d
Dziuperman <dziuperman@gmail.com>
2020-02-07 11:20:39 +1000

    Added Activity log

--- app/Customer.php
+++ app/Customer.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Customer extends Model
 {
+    use LogsActivity;
+
     protected $guarded = [];
+
+    protected static $logAttributes = ['name', 'email', 'phone', 'website'];
 }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ Route::group(['prefix' => 'auth'], function ($router) {
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-
 });
 
 Route::group(['middleware' => 'jwt.auth'], function ($router) {
